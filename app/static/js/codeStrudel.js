5189b1d148ae6853497be65b14f919034f0de537
caalma <nds.suscript@gmail.com>
2024-07-24 11:23:41 -0300

    CodeStrudel modo de archivado.

--- app/static/js/codeStrudel.js
+++ app/static/js/codeStrudel.js
@@ -1,26 +1,29 @@
 const CodeStrudel = CS = {
     elem: undefined,
-    arc: undefined,
+    arc_cnt: undefined,
+    arc_inp: undefined,
+    arc_lis: undefined,
     activar: () => {
         CS.elem = document.getElementById('containerStrudel');
         if(! CS.elem) return true;
-        CS.arc = CS.elem.querySelector('.archivo');
+        CS.arc_cnt = CS.elem.querySelector('.archivo');
+        CS.arc_inp = CS.arc_cnt.querySelector('input');
+        CS.arc_lis = CS.arc_cnt.querySelector('datalist');
     },
     leer: path => {
         if(path == undefined){
-            path = CS.arc.textContent;
+            path = CS.arc_inp.value;
         }else{
-            CS.arc.textContent = path;
+            CS.arc_inp.value = path;
         }
         CC.leer(`${path_code_strudel}${path}`, '', repl_code);
     },
     grabar: path => {
         if(path == undefined){
-            path = CS.arc.textContent;
+            path = CS.arc_inp.value;
         }else{
-            CS.arc.textContent = path;
+            CS.arc_inp.value = path;
         }
         CC.grabar(`${path_code_strudel}${path}`, '', repl.editor.code);
-    }
-
+    },
 }
